a8167bdf1b1a3706f2ce6d643f53a50455ddbab5
Tanner Barlow <tanner.barlow.dev@gmail.com>
2021-06-03 14:46:12 -0600

    fix: Update title and description from index.html

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import "./App.scss";
 import { HashRouter as Router } from "react-router-dom";
 import { MainContentRouter, Header } from "./components";
 import { pages } from "./pages";
+import { Helmet } from "react-helmet";
 
 const App: React.FC = () => {
 
   return (
       <div className={`app`}>
+        <Helmet>
+          <title>{"H & L Concrete"}</title>
+          <meta name="description" content={"H & L Concrete website"}/>
+        </Helmet>
         <Router>
           <Header/>
           <MainContentRouter
